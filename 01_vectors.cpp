d240077c725e6c87b6dc5ce02224b41bd1ff6745
Alexey Gerasev <alexey.gerasev@gmail.com>
2019-09-05 16:22:32 +0700

    update vectors

--- 01_vectors.cpp
+++ 01_vectors.cpp
@@ -1,84 +1,104 @@
 #include <iostream>
 #include <cmath>
+#include <cassert>
+
+class Vec2;
+class PVec2;
 
 
-class CartesianVector2 {
+class Vec2 {
 private:
     double x;
     double y;
 
 public:
-    CartesianVector2(double x, double y) {
-        this->x = x;
-        this->y = y;
-    }
-    /*
-    static CartesianVector2 from_polar(PolarVector2 p) {
-        return CartesianVector2(p.get_x(), p.get_y());
-    }
-    */
-    double get_x() {
-        return this->x;
-    }
-    double get_y() {
-        return this->y;
-    }
+    Vec2(double x, double y);
+    static Vec2 from_polar(PVec2 p);
+    
+    double get_x();
+    double get_y();
 
-    double get_r() {
-        return sqrt(x*x + y*y);
-    }
-    double get_phi() {
-        return atan2(y, x);
-    }
-
-    double length() {
-        return get_r();
-    }
-    double angle() {
-        return get_phi();
-    }
+    double get_r();
+    double get_phi();
 };
 
-class PolarVector2 {
+class PVec2 {
 private:
     double r;
     double phi;
 
 public:
-    PolarVector2(double r, double phi) {
-        this->r = r;
-        this->phi = phi;
-    }
+    PVec2(double r, double phi);
+    static PVec2 from_cartesian(Vec2 p);
 
-    static PolarVector2 from_cartesian(CartesianVector2 p) {
-        return PolarVector2(p.get_r(), p.get_phi());
-    }
+    double get_x();
+    double get_y();
 
-    double get_x() {
-        return r*cos(phi);
-    }
-    double get_y() {
-        return r*sin(phi);
-    }
-
-    double get_r() {
-        return r;
-    }
-    double get_phi() {
-        return phi;
-    }
-
-    double length() {
-        return get_r();
-    }
-    double angle() {
-        return get_phi();
-    }
+    double get_r();
+    double get_phi();
 };
 
+
+Vec2::Vec2(double x, double y) {
+    this->x = x;
+    this->y = y;
+}
+Vec2 Vec2::from_polar(PVec2 p) {
+    return Vec2(p.get_x(), p.get_y());
+}
+
+double Vec2::get_x() {
+    return this->x;
+}
+double Vec2::get_y() {
+    return this->y;
+}
+
+double Vec2::get_r() {
+    return sqrt(x*x + y*y);
+}
+double Vec2::get_phi() {
+    return atan2(y, x);
+}
+
+PVec2::PVec2(double r, double phi) {
+    this->r = r;
+    this->phi = phi;
+}
+
+PVec2 PVec2::from_cartesian(Vec2 p) {
+    return PVec2(p.get_r(), p.get_phi());
+}
+
+double PVec2::get_x() {
+    return r*cos(phi);
+}
+double PVec2::get_y() {
+    return r*sin(phi);
+}
+
+double PVec2::get_r() {
+    return r;
+}
+double PVec2::get_phi() {
+    return phi;
+}
+
+bool fcmp(double a, double b, double eps=1e-8) {
+    return fabs(a - b) < eps;
+}
+
 int main() {
-    PolarVector2 v = PolarVector2::from_cartesian(CartesianVector2(3, 4));
-    std::cout << v.get_x() << " " << v.get_y() << std::endl;
-    std::cout << v.length() << " " << v.angle() << std::endl;
+    double x = 3.0, y = 4.0;
+    PVec2 pv = PVec2::from_cartesian(Vec2(x, y));
+    assert(fcmp(x, pv.get_x()));
+    assert(fcmp(y, pv.get_y()));
+
+    double r = 1.0, phi = M_PI/3.0;
+    Vec2 v = Vec2::from_polar(PVec2(r, phi));
+    assert(fcmp(r, v.get_r()));
+    assert(fcmp(phi, v.get_phi()));
+
+    std::cout << "Complete!" << std::endl;
     return 0;
 }
